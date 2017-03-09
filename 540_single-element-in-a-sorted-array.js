e34fffc62f7e2a1accd8c912a343715eac31679d
YCmove <y8765gd@gmail.com>
2017-03-10 02:11:20 +0800

    540. Single Element in a Sorted Array

---
+++ 540_single-element-in-a-sorted-array.js
@@ -0,0 +1,32 @@
+/* 
+ ** Leetcode #540:
+ ** Language: Javascript
+ ** Problem: https://leetcode.com/problems/single-element-in-a-sorted-array/
+ ** Author: YCmove
+ ** Run Time: 
+ **/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var counter = (function () {
+    var count = 0;
+    return function () {
+        return count++
+    }
+})();
+
+
+var singleNonDuplicate = function (nums) {
+    var count = counter();
+    var num = nums[count];
+    var arr = nums.filter(model => model === num)
+    if (arr.length === 1) {
+        return num;git
+    }
+    return singleNonDuplicate(nums)
+
+};
+
+console.log(singleNonDuplicate([0, 1, 1]))
