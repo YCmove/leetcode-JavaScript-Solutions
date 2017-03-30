ae443e4e8b1be79b8ace53b431cfd223fc223727
YCmove <y8765gd@gmail.com>
2017-03-30 17:52:25 +0800

    7 reverse number

--- 7_reverseNumber.js
+++ 7_reverseNumber.js
@@ -4,27 +4,27 @@
  ** Language: Javascript
  ** Problem: https://leetcode.com/problems/reverse-integer
  ** Author: YCmove
- ** Run Time: 366ms
+ ** Run Time: 158ms
  **/
 /**
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {
+var reverse = function (x) {
     var newNum = x.toString().split('').reverse()
-    
+
     if (x < 0) {
         newNum.pop()
 
     }
-    
+
     var joinedNum = newNum.join('')
-    
+
     if (+joinedNum >= 2147483648) {
         return 0;
     }
-    
+
     return x < 0 ? -joinedNum : +joinedNum
 
-    
+
 };
