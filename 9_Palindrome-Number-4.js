02f25c5aff8e48c9b716a52ece09f306cf8a32e1
YCmove <y8765gd@gmail.com>
2017-03-30 18:13:18 +0800

    change to Math.floor

--- 9_Palindrome-Number-4.js
+++ 9_Palindrome-Number-4.js
@@ -7,17 +7,19 @@ var isPalindrome = function (x) {
     if (x < 0 || x > Math.pow(2, 32) - 1) return false
     if (x < 10) return true
 
-    var keepNumber = x
+    var originNumber = x
     var recNumber = x % 10
     x = parseInt(x / 10)
 
     while (x != 0) {
-        recNumber = recNumber * 10;
-        recNumber = recNumber + x % 10;
-        x = parseInt(x / 10);
+        recNumber = recNumber * 10 + x % 10;
+        x = Math.floor(x / 10);
+        // console.log('recNumber and x : ', recNumber, x)
     }
-    return recNumber == keepNumber;
+    return recNumber == originNumber;
 
 };
 
 module.exports = isPalindrome
+
+isPalindrome(34567)
